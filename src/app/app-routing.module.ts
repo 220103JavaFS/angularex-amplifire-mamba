cf3f9670cddef1598571a1adb1cae3bd79a8db9b
rfloyd01 <lynrdskynrd05@gmail.com>
2022-02-02 15:51:20 -0500

    added Mahad module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ZachComponent } from './components/zachcomponent/zachcomponent.component';
 import { BobbyComponentComponent } from './components/bobby-component/bobby-component.component';
+import { MahadComponent } from './components/mahad/mahad.component';
 
 const routes: Routes = [{
   path:"bobbyComponent",
@@ -9,6 +10,9 @@ const routes: Routes = [{
 }, {
   path:"zachComponent",
   component:ZachComponent
+}, {
+  path:"mahad",
+  component:MahadComponent
 }];
 
 @NgModule({
