dde01bb852bd2df9cb2a638f20b9add252b390a9
zbyrski <zachary.byrski@revature.net>
2022-02-02 12:52:40 -0800

    Cleaned up zachcomponent a bit

--- src/app/components/zachcomponent/zachcomponent.component.ts
+++ src/app/components/zachcomponent/zachcomponent.component.ts
@@ -11,8 +11,8 @@ export class ZachComponent implements OnInit {
 
   name:string = "Zach Byrski";
   home:string = "Washington, USA";
-  interests:string[] = ["Video Games", "Marvel Cinematic Universe", "Useless facts no one needs to know", 
-  "Rewatching shows I've already seen", "Catching pokemon"];
+  interests:string[] = ["Video Games", " Marvel Cinematic Universe", " Useless facts no one needs to know", 
+  " Rewatching shows I've already seen", " Catching pokemon"];
   quote:string = "My Bed is a magical place where I suddenly remember everything I forgot to do.";
 
   constructor() {
